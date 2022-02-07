44f721805583209ddfbdf70c1bd23e0f615db141
Kevin Johr <kevin.johr@posteo.de>
2022-02-07 11:25:41 +0100

    fix some bugs

--- js/backlog.js
+++ js/backlog.js
@@ -1,1 +1,9 @@
+let users = [];
+let text = [];
 setURL('http://gruppe-164.developerakademie.net/smallest_backend_ever');
+
+async function init() {
+    await downloadFromServer();
+    users = JSON.parse(backend.getItem('users')) || [];
+    text = JSON.parse(backend.getItem('text')) || [];
+}
