9c757b23443ebdffd6cd7ef2c31e49a60645297a
Kevin Johr <kevin.johr@posteo.de>
2022-02-14 14:04:36 +0100

    add showBacklogFunction !!BUG!!

--- js/backlog.js
+++ js/backlog.js
@@ -1,6 +1,28 @@
 
-async function showBacklog() {
-content_backlog = document.getElementById('backlog-content').innerHTML;
-
-
+function showBacklog() {
+let content_backlog = document.getElementById('backlog-content').innerHTML;
+    for (let index = 0; index < informations.length; index++) {
+        const element = informations[index];
+        document.getElementById('backlog-content').innerHTML += `
+        <div class="backlog-content-container-flex">
+        <div class="assignedToColor">
+            Farbe
+        </div>
+        <div class="assignedToUserImage">
+            Bild
+        </div>
+        <div class="assignedToPerson">
+            <div class="assignedToName"> Name </div>
+            <a href="mailto:Email@email.com">Email@email.com</a>
+        </div>
+        </div>
+        <div class="assignedToCategory">
+        Category
+        </div>
+        <div class="assignedToDetails">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam consequatur saepe accusamus excepturi
+        omnis similique sunt vel magnam nemo facilis.
+        </div>
+        `
+    }
 }
